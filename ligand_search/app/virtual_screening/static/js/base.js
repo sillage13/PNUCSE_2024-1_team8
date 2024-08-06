ab5e327f631599119658bbcaae23d0e848189d37
gabin <bini0408@pusan.ac.kr>
2024-08-06 10:23:00 +0900

    일부 js 코드 이동

--- ligand_search/app/virtual_screening/static/js/base.js
+++ ligand_search/app/virtual_screening/static/js/base.js
@@ -30,20 +30,6 @@ $(document).ready(function() {
         }
     })
 
-    //드롭 다운 메뉴 구현
-    $('label[for=method]').click(function() {
-        $('.select_ul').toggle()
-        $('#arrow_drop_down').toggleClass('flip')
-    })
-
-    $('.select_li').click(function() {
-        let method = $(this).text()
-        $('#method').val(method)
-        $('label[for=method]').text(method)
-        $('label[for=method]').css({'color':'var(--on-container)'})
-        $('label[for=method]').click()
-    })
-
     //파일 선택 시 이름 표시
     $('#id_receptor, #id_ligand').change(function(event) {
         let filename = $(this).val()
--- ligand_search/app/virtual_screening/static/js/search.js
+++ ligand_search/app/virtual_screening/static/js/search.js
@@ -2,4 +2,20 @@ $(document).ready(function() {
     //폼에 element 추가
     $('.form_ul > li:first-child').prepend('<label class="label">Receptor</label>')
     $('.form_ul > li:first-child').append('<span class="material-symbols-outlined">upload</span>')
+
+    $('#method').val(null)
+    
+    //드롭 다운 메뉴 구현
+    $('label[for=method]').click(function() {
+        $('.select_ul').toggle()
+        $('#arrow_drop_down').toggleClass('flip')
+    })
+
+    $('.select_li').click(function() {
+        let method = $(this).text()
+        $('#method').val(method)
+        $('label[for=method]').text(method)
+        $('label[for=method]').css({'color':'var(--on-container)'})
+        $('label[for=method]').click()
+    })
 })
