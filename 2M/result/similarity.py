6336337ae6644eec4e57581def0180e43888718f
JeongMin <jmin5854@gmail.com>
2024-09-23 15:22:16 +0900

    2M 정리, result

--- 2M/result/similarity.py
+++ 2M/result/similarity.py
@@ -3,6 +3,7 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 from rdkit import DataStructs
 from rdkit.Chem import rdFMCS
+import re
 import argparse
 
 def calculate_similarity_rdkit(smiles1, smiles2, fp_type='maccs'):
@@ -43,27 +44,40 @@ def calculate_similarity_scaffolds(smiles1, smiles2):
 def load_data(filename):
     with open(filename, 'rb') as f:
         return pickle.load(f)
+    
+
+def extract_numbers_from_input():
+    numbers = []
+    print("입력")
+    for _ in range(10):
+        # 사용자로부터 입력을 받음
+        line = input()
+        # 정규 표현식을 사용하여 ligand 뒤의 숫자만 추출
+        match = re.search(r'ligand(\d+)', line)
+        if match:
+            numbers.append(int(match.group(1)))  # 추출된 숫자를 정수로 변환하여 리스트에 추가
+    return numbers
 
 
-def main(result_filename):
-    smile_dict_filename = './../data/smile_dict.dat'
-    smile_dict = load_data(smile_dict_filename)
-    tmp = load_data(result_filename)
-    result = tmp[:10]
-    print(result)
-    print(len(result))
+if __name__ == "__main__":
+    index_list = extract_numbers_from_input()
+    smile_iist = load_data('./../data/smile_list.dat')
+
+    print(index_list)
+    print(len(index_list))
     similarity = []
     msc = []
     scaffolds = []
     # 클러스터 결과 유사도 계산
-    for i in range(len(result)):
-        for j in range(i + 1, len(result)):
-            smiles1 = smile_dict[result[i][1]]
-            smiles2 = smile_dict[result[j][1]]
+    for i in range(len(index_list)):
+        for j in range(i + 1, len(index_list)):
+            smiles1 = smile_iist[index_list[i]]
+            smiles2 = smile_iist[index_list[j]]
             # print(1)
             similarity.append(calculate_similarity_rdkit(smiles1, smiles2))
+            print(similarity[-1])
             # print(2)
-            msc.append(calculate_similarity_mcs(smiles1, smiles2))
+            # msc.append(calculate_similarity_mcs(smiles1, smiles2))
             # print(3)
             # scaffolds.append(calculate_similarity_scaffolds(smiles1, smiles2))
             # print("WORK")
@@ -73,10 +87,10 @@ def main(result_filename):
     print(f"Average similarity: {sum(similarity) / len(similarity):.2f}")
     print(f"Minimum similarity: {min(similarity):.2f}")
 
-    print("최대 공통 부분구조 기반 유사도")
-    print(f"Maximum similarity: {max(msc):.2f}")
-    print(f"Average similarity: {sum(msc) / len(msc):.2f}")
-    print(f"Minimum similarity: {min(msc):.2f}")
+    # print("최대 공통 부분구조 기반 유사도")
+    # print(f"Maximum similarity: {max(msc):.2f}")
+    # print(f"Average similarity: {sum(msc) / len(msc):.2f}")
+    # print(f"Minimum similarity: {min(msc):.2f}")
 
     # print("구조 키 기반 유사도")
     # print(f"Average similarity: {sum(scaffolds) / len(scaffolds):.2f}")
@@ -84,9 +98,4 @@ def main(result_filename):
     # print(f"Minimum similarity: {min(scaffolds):.2f}")
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Calculate molecular similarities.')
-    parser.add_argument('cluster_result_filename', type=str, help='The filename of the cluster result data file.')
 
-    args = parser.parse_args()
-    main(args.cluster_result_filename)
