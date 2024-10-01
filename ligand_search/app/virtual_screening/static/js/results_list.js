81e414fd9b16b3da380f360d3f05bfc35f4d8cb7
gabin <bini0408@pusan.ac.kr>
2024-10-01 16:09:51 +0900

    결과 리스트 페이지 디자인 수정

--- ligand_search/app/virtual_screening/static/js/results_list.js
+++ ligand_search/app/virtual_screening/static/js/results_list.js
@@ -1,2 +1,33 @@
 $(document).ready(function() {
+    $('.result_receptor, .result_method').hover(
+        function() {
+            if ($(this).hasClass('result_receptor')) 
+                var sibling = '.result_method'
+            else
+                var sibling = '.result_receptor'
+
+            var txt = $(this).find(".hidden-overflow")
+            console.log(txt[0].clientWidth)
+            console.log(txt[0].scrollWidth)
+            if (txt[0].clientWidth < txt[0].scrollWidth) {
+                $(this).addClass('show-text')
+                $(this).siblings(sibling).hide()
+
+                if (txt[0].clientWidth < txt[0].scrollWidth) {
+                    txt.addClass("flow-text")
+                }
+            }
+        },
+        function() {
+            if ($(this).hasClass('result_receptor'))
+                var sibling = '.result_method'
+            else
+                var sibling = '.result_receptor'
+
+            $(this).removeClass('show-text')
+            $(this).siblings(sibling).show()
+
+            $(this).find(".hidden-overflow").removeClass("flow-text")
+        }
+    )
 })
