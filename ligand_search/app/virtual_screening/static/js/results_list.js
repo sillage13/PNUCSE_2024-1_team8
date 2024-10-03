946b73fa9727b978335e642466bd23eebe72882d
gabin <bini0408@pusan.ac.kr>
2024-10-03 16:10:02 +0900

    hover 시 가려진 글자가 보이는 기능 추가

--- ligand_search/app/virtual_screening/static/js/results_list.js
+++ ligand_search/app/virtual_screening/static/js/results_list.js
@@ -7,14 +7,13 @@ $(document).ready(function() {
                 var sibling = '.result_receptor'
 
             var txt = $(this).find(".hidden-overflow")
-            console.log(txt[0].clientWidth)
-            console.log(txt[0].scrollWidth)
             if (txt[0].clientWidth < txt[0].scrollWidth) {
                 $(this).addClass('show-text')
                 $(this).siblings(sibling).hide()
 
                 if (txt[0].clientWidth < txt[0].scrollWidth) {
                     txt.addClass("flow-text")
+                    $('.flow-text').css('animation-duration', txt[0].scrollWidth/30+'s')
                 }
             }
         },
