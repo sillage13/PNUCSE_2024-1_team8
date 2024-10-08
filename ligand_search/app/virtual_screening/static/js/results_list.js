3bba09bed30af1abc1ab5f17645ccf484e4ecad0
gabin <bini0408@pusan.ac.kr>
2024-10-08 23:17:00 +0900

    hover가 불가능한 환경에서 모든 글자를 확인할 수 있게 수정

--- ligand_search/app/virtual_screening/static/js/results_list.js
+++ ligand_search/app/virtual_screening/static/js/results_list.js
@@ -1,32 +1,43 @@
 $(document).ready(function() {
-    $('.result_receptor, .result_method').hover(
-        function() {
-            if ($(this).hasClass('result_receptor')) 
-                var sibling = '.result_method'
-            else
-                var sibling = '.result_receptor'
+    if (window.matchMedia('(hover:hover) and (pointer:fine').matches) {
+        $('.result_receptor, .result_method').hover(
+            function() {
+                if ($(this).hasClass('result_receptor')) 
+                    var sibling = '.result_method'
+                else
+                    var sibling = '.result_receptor'
 
+                var txt = $(this).find(".hidden-overflow")
+                if (txt[0].clientWidth < txt[0].scrollWidth) {
+                    $(this).addClass('show-text')
+                    $(this).siblings(sibling).hide()
+
+                    if (txt[0].clientWidth < txt[0].scrollWidth) {
+                        txt.addClass("flow-text")
+                        $('.flow-text').css('animation-duration', txt[0].scrollWidth/30+'s')
+                    }
+                }
+            },
+            function() {
+                if ($(this).hasClass('result_receptor'))
+                    var sibling = '.result_method'
+                else
+                    var sibling = '.result_receptor'
+
+                $(this).removeClass('show-text')
+                $(this).siblings(sibling).show()
+
+                $(this).find(".hidden-overflow").removeClass("flow-text")
+            }
+        )
+    }
+    else {
+        $('.result_receptor, .result_method').each(function() {
             var txt = $(this).find(".hidden-overflow")
             if (txt[0].clientWidth < txt[0].scrollWidth) {
-                $(this).addClass('show-text')
-                $(this).siblings(sibling).hide()
-
-                if (txt[0].clientWidth < txt[0].scrollWidth) {
-                    txt.addClass("flow-text")
-                    $('.flow-text').css('animation-duration', txt[0].scrollWidth/30+'s')
-                }
+                txt.addClass("flow-text")
+                $('.flow-text').css('animation-duration', txt[0].scrollWidth/30+'s')
             }
-        },
-        function() {
-            if ($(this).hasClass('result_receptor'))
-                var sibling = '.result_method'
-            else
-                var sibling = '.result_receptor'
-
-            $(this).removeClass('show-text')
-            $(this).siblings(sibling).show()
-
-            $(this).find(".hidden-overflow").removeClass("flow-text")
-        }
-    )
+        })
+    }
 })
